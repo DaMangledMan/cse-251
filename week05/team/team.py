705bdef11f8fda1650a3e1e58be56c6ab42f6a33
DaMangledMan <jacobh52003@gmail.com>
2024-05-22 14:38:37 -0600

    finished team activity

--- week05/team/team.py
+++ week05/team/team.py
@@ -18,6 +18,7 @@ import threading
 import multiprocessing as mp
 import random
 from os.path import exists
+import queue
 
 
 
@@ -42,8 +43,44 @@ def is_prime(n: int) -> bool:
     return True
 
 # TODO create read_thread function
+class Request_thread(threading.Thread):
 
+    def __init__(self, file_name, Q: mp.Queue, sem: mp.Semaphore):
+        # Call the Thread class's init function
+        threading.Thread.__init__(self)
+        self.file_name = file_name
+        self.Q = Q
+        self.sem = sem
+
+    def run(self):
+        self.file = open(rf"{self.file_name}")
+        print("thread start")
+        while True:
+            print("thread continue")
+            number = self.file.readline()
+            number = number[:-1]
+            if number == "":
+                self.Q.put(None)
+                self.sem.release()
+                break
+            self.Q.put(int(number))
+            self.sem.release()
+
+            
 # TODO create prime_process function
+def prime_process(Q: mp.Queue, sem: mp.Semaphore, primes):
+    print("process start")
+    while True:
+        print("process continue")
+        sem.acquire()
+        number = Q.get()
+        if number == None:
+            Q.put(None)
+            sem.release()
+            break
+        if is_prime(number):
+            primes.append(number)
+
 
 def create_data_txt(filename):
     # only create if is doesn't exist 
@@ -63,14 +100,28 @@ def main():
     log.start_timer()
 
     # TODO Create shared data structures
+    primes = mp.Manager().list()
+    Q = mp.Queue()
+    process_sem = mp.Semaphore(0)
+    pr_list = []
 
     # TODO create reading thread
+    th = Request_thread(filename, Q, process_sem)
 
     # TODO create prime processes
+    for _ in range(PRIME_PROCESS_COUNT):
+        pr = mp.Process(target=prime_process, args=(Q, process_sem, primes))
+        pr_list.append(pr)
 
     # TODO Start them all
+    th.start()
+    for pr in pr_list:
+        pr.start()
 
     # TODO wait for them to complete
+    th.join()
+    for pr in pr_list:
+        pr.join()
 
     log.stop_timer(f'All primes have been found using {PRIME_PROCESS_COUNT} processes')
 
