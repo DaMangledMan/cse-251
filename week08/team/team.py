91a7a59a0a51d564278f9b8f5803c9e89475a594
DaMangledMan <jacobh52003@gmail.com>
2024-06-12 19:28:04 -0600

    worked on team 8

--- week08/team/team.py
+++ week08/team/team.py
@@ -21,7 +21,7 @@ def merge_sort(arr):
 
     # base case of the recursion - must have at least 2+ items
     if len(arr) > 1:
- 
+
          # Finding the mid of the array
         mid = len(arr) // 2
  
@@ -74,21 +74,117 @@ def merge_normal(arr):
 def merge_sort_thread(arr):
     # TODO - Add your code here to use threads.  Each time the merge algorithm does a recursive
     #        call, you need to create a thread to handle that call
-    pass
+    # base case of the recursion - must have at least 2+ items
+    if len(arr) > 1:
+ 
+         # Finding the mid of the array
+        mid = len(arr) // 2
+ 
+        # Dividing the array elements
+        L = arr[:mid]
+ 
+        # into 2 halves
+        R = arr[mid:]
+ 
+        # Sorting the first half
+        l_th = threading.Thread(merge_sort_thread, args=(L,))
+        l_th.start()
+ 
+        # Sorting the second half
+        r_th = threading.Thread(merge_sort_thread, args=(R,))
+        r_th.start()
+        
+        l_th.join()
+        r_th.join()
+
+        i = j = k = 0
+ 
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+ 
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+ 
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+    return arr
 
 
 # -----------------------------------------------------------------------------
 def merge_sort_process(arr):
     # TODO - Add your code here to use threads.  Each time the merge algorithm does a recursive
     #        call, you need to create a process to handle that call
-    pass
+    if len(arr) > 1:
+ 
+         # Finding the mid of the array
+        mid = len(arr) // 2
+ 
+        # Dividing the array elements
+        L = mp.Manager().list(arr[:mid])
+ 
+        # into 2 halves
+        R = mp.Manager().list(arr[mid:])
+ 
+        # Sorting the first half
+        l_pool = mp.Pool(1)
+        L = l_pool.apply_async(merge_sort_process, args=(L,))
+
+        # Sorting the second half
+        r_pool = mp.Pool(1)
+        R = r_pool.apply_async(merge_sort_process, args=(R,))
+
+        l_pool.close()
+        l_pool.join()
+        L.get()
+        r_pool.close()
+        r_pool.join()
+        R.get()
+
+        i = j = k = 0
+ 
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+ 
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+ 
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+    
+    return arr
+
 
 
 # -----------------------------------------------------------------------------
 def main():
     merges = [
         (merge_sort, ' Normal Merge Sort '), 
-        (merge_sort_thread, ' Threaded Merge Sort '), 
+        #(merge_sort_thread, ' Threaded Merge Sort '), 
         (merge_sort_process, ' Processes Merge Sort ')
     ]
 
